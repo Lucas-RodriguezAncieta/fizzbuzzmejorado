f95bfc63aafecfed53a255f16618ed95d230acd6
Lucas-RodriguezAncieta <lucassantiago.lsra@gmail.com>
2025-09-08 09:52:35 -0400

    Implementar código mínimo para pasar el test del número 1

---
+++ src/fizzbuzz.js
@@ -0,0 +1,5 @@
+// fizzbuzz.js
+function fizzBuzz(num) {
+    return '1'; // hardcodeado para pasar el test
+}
+module.exports = fizzBuzz;
---
+++ src/fizzbuzz.spec.js
@@ -0,0 +1,6 @@
+// fizzbuzz.spec.js
+const fizzBuzz = require('./fizzbuzz');
+
+test('debería retornar 1 para el número 1', () => {
+    expect(fizzBuzz(1)).toBe('1');
+});
